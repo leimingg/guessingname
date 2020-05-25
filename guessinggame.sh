8da85455e73655a2b34c4554dadf75e5db0b7bc5
leimingg <lei568@gmail.com>
2020-05-25 11:12:15 -0700

    Update condition statement

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,12 +3,12 @@
 guessing()
 {
 		
-		if [ $guess -lt `ls -1 | wc -l` ];
+		if [ $guess -lt `ls -a |wc -l` ];
 		then
 		echo "Number is too low"
 		fi;
 
-		if [ $guess -gt `ls -1 | wc -l` ];
+		if [ $guess -gt `ls -a | wc -l` ];
 		then
 		echo "Number is too high"
 		fi;
@@ -18,11 +18,12 @@ guessing()
 count=1
 while [ $count -ne '0' ]
 do
+echo -n "#"$count
 echo -n "This simple program checks how many files are in the current directory"
 echo ""
 echo -n "Please enter your guess number:"
 read guess 
-if [ $guess -eq `ls -1 | wc -l` ];
+if [ $guess -eq `ls -a | wc -l` ];
 		then
 		echo "Congrats! It is correct"
 		break;
--- makefile
+++ makefile
@@ -1,8 +1,10 @@
 all: README.md
 
 README.md:
-	echo "guessinggame.sh for Unix Workbench class\n">>README.md
+	echo "guessinggame.sh for Unix Workbench class">>README.md
+	echo ""
 	echo -n "Successfully created README.md">>README.md
+	echo ""
 	date>>README.md
 	echo -n "\nNumber of lines in guessinggame.sh:">>README.md
 	wc -l guessinggame.sh |egrep -o "[0-9]+" >> README.md
